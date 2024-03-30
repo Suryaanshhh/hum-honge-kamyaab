cb93b88b1bdd9bc76bfb324c74ce0bb77742bd28
Suryaanshhh <153890779+Suryaanshhh@users.noreply.github.com>
2024-03-30 14:55:47 +0530

    Day 1 coding

---
+++ start.js
@@ -0,0 +1,78 @@
+//Basics of coding 
+
+//Loops :-Doing work in a certain manner untill we get desired results
+
+//1-Entry controlled -condition checked first
+
+//2-Exit controlled -Code executed first 
+
+//while loops
+//initialisation (let i=1)
+//while(i<=5)
+//increament / decrement 
+
+//print table of 5 
+
+// let i=5;
+// while(i<=50){
+//     console.log(i);
+//     i=i+5
+// }
+
+//do while loop 
+
+// let x="comiited";
+// do{
+//   console.log("I love you")
+// }
+// while(x=="single")
+
+
+// java script is dynamically defined;
+
+//Integer Division
+
+//Math.floor=Round off to previous integer
+
+//Math.Ceil=Round off to next integer
+
+//how to find digits using while loop
+
+// while(n>0){
+//     console.log(n%10);
+//     n=Math.floor(n/10)
+//   }
+  
+//Armstrong Number 
+
+// var sum=0;
+// while(n>0){
+//  let x= n%10
+//   n=Math.floor(n/10);//n=n+1
+//   sum= sum +x*x*x
+// }
+// console.log(sum)
+
+//question reverse digit 
+//input 123 //std ouput =3 2 1
+//output 1 2 3 
+
+
+//For loops 
+
+// //for(initialisation;stopping condition ; progression i.e., increment/decrement){
+//     code to be executed
+// }
+
+
+// pattern question
+// for(var i=1;i<=4;i=i+1){
+//     let str=""
+//     for(var j=1;j<=i;j=j+1)
+//     {
+//       str=str+' '
+//     }
+//     console.log(str);
+//   }
+
+
